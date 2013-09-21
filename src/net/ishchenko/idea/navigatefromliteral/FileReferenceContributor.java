509c8a604ac51ce713f83e8e3d51138735952814
Max Ishchenko <max.ishchenko@gmail.com>
2013-09-21 14:39:34 +0400

    Checking if PsiLiteral is present allowing for better JetBrains platform support. Fixes #4.

--- src/net/ishchenko/idea/navigatefromliteral/FileReferenceContributor.java
+++ src/net/ishchenko/idea/navigatefromliteral/FileReferenceContributor.java
@@ -1,6 +1,6 @@
 package net.ishchenko.idea.navigatefromliteral;
 
-import com.intellij.patterns.PsiJavaPatterns;
+import com.intellij.patterns.StandardPatterns;
 import com.intellij.patterns.XmlPatterns;
 import com.intellij.psi.*;
 import com.intellij.psi.xml.XmlAttributeValue;
@@ -18,12 +18,19 @@ public class FileReferenceContributor extends PsiReferenceContributor {
 
     @Override
     public void registerReferenceProviders(PsiReferenceRegistrar registrar) {
-        registrar.registerReferenceProvider(PsiJavaPatterns.psiLiteral(), new PsiReferenceProvider() {
-            @NotNull
-            public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
-                return new PsiReference[]{new OneWayPsiFileFromPsiLiteralReference((PsiLiteral) element)};
-            }
-        });
+
+        try {
+            Class.forName("com.intellij.psi.PsiLiteral", false, getClass().getClassLoader());
+            registrar.registerReferenceProvider(StandardPatterns.instanceOf(PsiLiteral.class), new PsiReferenceProvider() {
+                @NotNull
+                public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+                    return new PsiReference[]{new OneWayPsiFileFromPsiLiteralReference((PsiLiteral) element)};
+                }
+            });
+        } catch (ClassNotFoundException e) {
+            //Ok, then. Some JetBrains platform IDE that has no Java support.
+        }
+
         registrar.registerReferenceProvider(XmlPatterns.xmlAttributeValue(), new PsiReferenceProvider() {
             @NotNull
             @Override
